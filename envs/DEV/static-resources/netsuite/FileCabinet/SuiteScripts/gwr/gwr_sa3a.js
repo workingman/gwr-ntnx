3766a0ca66813db75372c8d68f544d66e97baf2c
Geoffrey Routledge <geoff@dgbi.ca>
2023-11-10 06:54:16 +0000

    baseline

---
+++ envs/DEV/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_sa3a.js
@@ -0,0 +1,27 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/log'],
+    /**
+ * @param{log} log
+ */
+
+    // 20230927-004446
+    // 20231109-151128
+       // 20231102-142207 
+    (log) => {
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => {
+
+        }
+
+        return {onRequest}
+
+    });
