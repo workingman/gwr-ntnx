3e1cbd685fe3b2cd458cc426b93ae97869654e3e
Geoffrey Routledge <geoff@dgbi.ca>
2023-11-20 20:34:22 +0000

    Target environment fetch updates

---
+++ envs/UAT/static-resources/netsuite/FileCabinet/SuiteScripts/mkd/mkd_showmessage.js
@@ -0,0 +1,14 @@
+/**
+ * @NScriptType ClientScript
+ * @NApiVersion 2.x
+ */
+define([], function() {
+    function showMessage() {
+        alert('This is just a placeholder.  Edit this popup message before deploying!');
+    }
+    
+    return {
+        pageInit: showMessage
+    }
+});
+
