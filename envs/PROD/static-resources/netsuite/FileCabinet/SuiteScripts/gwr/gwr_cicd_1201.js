d7b2c8f39743851330795c574e6b6d66e120b8b0
Geoffrey Routledge <geoff@dgbi.ca>
2023-12-05 19:11:15 +0000

    Promotion of "BIZAPP-0005"

--- envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_1201.js
+++ envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_1201.js
@@ -10,7 +10,7 @@ define(['N/record', 'N/search'],
  * @param{search} search
  */
 function(record, search) {
-    // 20231201-092345 gr conflicting change 20231204-101212
+    // 20231201-092345 gr conflicting change 20231204-101212  kgr change 20231205-1056 BIZAPP-0005
     
     /**
      * Function to be executed after page is initialized.
@@ -28,11 +28,15 @@ function(record, search) {
 // gwr non conflicting 20231204-083705
 // gwr non confcliting 20231204-101225
 // gwr non conflicting 20231205-101937
+// gwr non conflicting change 20231205-105641
     return {
         pageInit: pageInit,
     };
     // km non conflicting 1 20231204-100134
     // kgr nonconflicting 20231205-1019
     // kgr nonconflicting 20231205-1023 update to same feature branch
+    // kgr non conflicting change 20231205-1056 BIZAPP-0005
+    // kgr non conflicting change 20231205-1056 BIZAPP-0005 rework
+
 
 });
