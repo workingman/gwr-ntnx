f69e2f43bc3bbb3bdc6990a79a622472ea840c25
Geoffrey Routledge <geoff@dgbi.ca>
2023-12-05 18:52:00 +0000

    Target environment fetch updates

---
+++ envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/nitzan_script.js
@@ -0,0 +1,6 @@
+console.log('hello remote!!! what happened to a simple hellow world?  done on salto UI')
+console.log('Hey, remote!!! done on netsuite UI')
+console.log('this script has been modified by SJP for a customer example')
+let a = 211
+let b = 12
+console.log(a)
