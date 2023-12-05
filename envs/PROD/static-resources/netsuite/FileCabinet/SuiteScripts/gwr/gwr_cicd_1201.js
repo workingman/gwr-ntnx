0b908f63275576237e12c6c849d514a5928d7168
Geoffrey Routledge <geoff@dgbi.ca>
2023-12-05 19:14:19 +0000

    Manual merge of deployment conflicts

--- envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_1201.js
+++ envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_1201.js
@@ -10,7 +10,7 @@ define(['N/record', 'N/search'],
  * @param{search} search
  */
 function(record, search) {
-    // 20231201-092345 gr conflicting change 20231204-101212  kgr change 20231205-1056 BIZAPP-0005
+    // 20231201-092345 gr conflicting change 20231204-101212
     
     /**
      * Function to be executed after page is initialized.
@@ -28,7 +28,6 @@ function(record, search) {
 // gwr non conflicting 20231204-083705
 // gwr non confcliting 20231204-101225
 // gwr non conflicting 20231205-101937
-// gwr non conflicting change 20231205-105641
     return {
         pageInit: pageInit,
     };
