e9765b3d9e18a891f8b5e73fa7f88769b44f1e6c
Geoffrey Routledge <geoff@dgbi.ca>
2023-12-11 21:00:18 +0000

    Target environment fetch updates

---
+++ envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/testSuiteApp/com.saltoio.salto/build/src/validation.js
@@ -0,0 +1,6 @@
+define(["require", "exports"], function (require, exports) {
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.validateInput = void 0;
+    exports.validateInput = function (validator, schema, input) { return validator.validate(schema, input); };
+});
+//# sourceMappingURL=validation.js.map
