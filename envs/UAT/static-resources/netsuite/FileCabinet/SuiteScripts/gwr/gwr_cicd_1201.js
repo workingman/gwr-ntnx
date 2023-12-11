ca343c70fcbd8d42ea97061f156f868173dfd4b8
Geoffrey Routledge <geoff@dgbi.ca>
2023-12-11 20:51:31 +0000

    BIZAPP-0009 rework-2
    This deployment was created from the following Pull Request: https://github.com/workingman/gwr-ntnx-sdf/pull/67 (0a2603a)
    • Source Pull Request: https://github.com/workingman/gwr-ntnx-sdf/pull/67
    • Target Environment: UAT
    • Author: Geoffrey Routledge
    • https://app.salto.io/orgs/d39e3801-354a-4f02-aedd-947684f9f9aa/envs/409252aa-2e5e-4a65-96af-8516068be9aa/deployments/643a3af6-a38c-43f8-a4e1-0fed30c2606f
    • Deployed files: UAT.netsuite.jsonl.zip,gwr_cicd_1201.js

--- envs/UAT/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_1201.js
+++ envs/UAT/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_1201.js
@@ -10,7 +10,7 @@ define(['N/record', 'N/search'],
  * @param{search} search
  */
 function(record, search) {
-    // 20231201-092345 gr conflicting change 20231211-121722 BIZAPP-0010 with rework1
+    // 20231201-092345 gr conflicting change 20231211-121722 BIZAPP-0010 with rework2
 
     /**
      * Function to be executed after page is initialized.
@@ -44,7 +44,6 @@ function(record, search) {
     // kgr non conflicting change 20231205-1056 BIZAPP-0007 change 1
     // kgr non conflicting change 20231205-1056 BIZAPP-0007 change 2
     // kgr non conflicting change 20231205-1056 BIZAPP-0007 change 3 additional edits as requested
-    // kgr non conflicting change 20231205-1056 BIZAPP-0009
-    // kgr non conflicting change 20231205-1056 BIZAPP-0009 rework1
-
+    // kgr non conflicting change 20231205-1056 BIZAPP-0009 rework1 rework2
+    // kgr non conflicting change 20231205-1056 BIZAPP-0009 rework2
 });
