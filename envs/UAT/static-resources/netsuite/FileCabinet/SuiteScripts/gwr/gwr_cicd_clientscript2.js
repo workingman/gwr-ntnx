a60b19a465885df20889b8fc1c3eba32b06d34f0
Geoffrey Routledge <geoff@dgbi.ca>
2024-01-16 23:53:53 +0000

    gr 154404
    This deployment was created from the following Pull Request: https://github.com/workingman/gwr-ntnx-sdf/pull/90 (47e2d85)
    • Source Pull Request: https://github.com/workingman/gwr-ntnx-sdf/pull/90
    • Target Environment: UAT
    • Author: Geoffrey Routledge
    • https://app.salto.io/orgs/d39e3801-354a-4f02-aedd-947684f9f9aa/envs/409252aa-2e5e-4a65-96af-8516068be9aa/deployments/20419a7f-c60a-4bdd-9715-34f65078d707
    • Deployed files: UAT.netsuite.json,gwr_cicd_clientscript2.js

--- envs/UAT/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_clientscript2.js
+++ envs/UAT/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_clientscript2.js
@@ -9,13 +9,17 @@ define(['N/record', 'N/search'],
  * @param{record} record
  * @param{search} search
  */
+
 // non-conflicting line GR 20231201-100259
        // 20231210-120823
        // 20231210-121100
        // 20231210-122829
        // BIZAPP-105707
        // BIZAPP-105707 post-PR-creation change
+       // BIZAPP-154404 gr
+
 function(record, search) {
+    // 20231212-112318
     
     /**
      * Function to be executed after page is initialized.
@@ -34,6 +38,11 @@ function(record, search) {
     return {
         pageInit: pageInit,
     };
-    // 20231130-180905
-    
+
+    // 20231130-175636
+    // 20231130-181935
+    // 20231130-182155
+    // KGR test 12/01/2023 Update for BIZAPP-0002 conflict additional updates for BIZAPP-0003
+       //kgr222
+
 });
