bc3f4d2727151c54158e854688a3a5fc61fb4dcc
Geoffrey Routledge <geoff@dgbi.ca>
2024-01-17 00:18:25 +0000

    154404
    Promoting 'Update gwr_cicd_clientscript2.js' and 'gr 154404'
    • Source: 2 previous deployments to the "UAT" environment
    • Target Environment: PROD
    • Author: Geoffrey Routledge
    • https://app.salto.io/orgs/d39e3801-354a-4f02-aedd-947684f9f9aa/envs/fc619771-f6ff-464e-a10e-a542c52136ef/deployments/e0eb7da8-862f-480a-b68c-c815de484182
    • Deployed files: PROD.netsuite.jsonl.zip,gwr_cicd_clientscript2.js

--- envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_clientscript2.js
+++ envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_cicd_clientscript2.js
@@ -10,6 +10,18 @@ define(['N/record', 'N/search'],
  * @param{record} record
  * @param{search} search
  */
+
+// non-conflicting line GR 20231201-100259
+       // 20231210-120823
+       // 20231210-121100
+       // 20231210-122829
+       // BIZAPP-105707
+       // BIZAPP-105707 post-PR-creation change
+       // BIZAPP-154404 gr
+
+       // BIZAPP-154404 rework 20240116-155442
+       // more rework 20240116-160105
+
 function(record, search) {
     
     /**
@@ -29,6 +41,13 @@ function(record, search) {
     return {
         pageInit: pageInit,
     };
+
+
     // 20231130-175636
-    
+    // 20231130-181935
+    // 20231130-182155
+    // KGR test 12/01/2023 Update for BIZAPP-0002 conflict additional updates for BIZAPP-0003
+       //kgr222
+
+
 });
