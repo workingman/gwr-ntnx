48bfca56b08100fed2a532522b4430a49134d08d
Geoffrey Routledge <geoff@dgbi.ca>
2024-02-27 20:32:33 +0000

    Target environment fetch updates

---
+++ envs/PROD/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr_sa3a.js
@@ -0,0 +1,28 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define(['N/log'],
+    /**
+ * @param{log} log
+ */
+
+    // 20230927-004446
+       // 20231102-142207km 
+       // 20231110-123137km
+       // 20231116-234542km
+    (log) => {
+        /**
+         * Defines the Suitelet script trigger point.
+         * @param {Object} scriptContext
+         * @param {ServerRequest} scriptContext.request - Incoming request
+         * @param {ServerResponse} scriptContext.response - Suitelet response
+         * @since 2015.2
+         */
+        const onRequest = (scriptContext) => {
+
+        }
+
+        return {onRequest}
+// 20231213-094129
+    });
