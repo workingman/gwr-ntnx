a699993fc51d1da1160070dc382c02ec64be85d6
Geoffrey Routledge <geoff@dgbi.ca>
2024-08-27 18:50:02 +0000

    BIZAPP-650 baseline commit

--- envs/UAT/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr1.js
+++ envs/UAT/static-resources/netsuite/FileCabinet/SuiteScripts/gwr/gwr1.js
@@ -12,6 +12,7 @@ define(['N/record', 'N/search'],
  */
 function(record, search) {
     // GR 20240313-123224
+    // GR 20240503-132414
     /**
      * Function to be executed after page is initialized.
      *
@@ -22,9 +23,7 @@ function(record, search) {
      * @since 2015.2
      */
     function pageInit(scriptContext) {
-
-        const s = "20240313-130450"
-
+        const s = "20240313-122646"
         alert("Bonjour tout la monde")
 
     }
